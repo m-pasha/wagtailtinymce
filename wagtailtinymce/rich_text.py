333590bfcbe1ad0a2705b6e130dc58a14ca2f7fa
m-pasha <pv.pasha.pv@gmail.com>
2019-06-13 17:56:00 +0200

    fixed 'rerderer' arg in render

--- wagtailtinymce/rich_text.py
+++ wagtailtinymce/rich_text.py
@@ -78,7 +78,7 @@ class TinyMCERichTextArea(WidgetWithScript, widgets.Textarea):
     def get_panel(self):
         return RichTextFieldPanel
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, renderer=None):
         if value is None:
             translated_value = None
         else:
